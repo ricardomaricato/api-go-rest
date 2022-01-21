45cf420259fe2a543f8f675037f4e9c3bc47cd59
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-21 15:59:22 -0300

    feat: respondendo JSON

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/ricardomaricato/api-go-rest/models"
 	"github.com/ricardomaricato/api-go-rest/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "nome 1", Historia: "Historia 1"},
+		{Nome: "nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Iniciando o servidor Resto com Go")
 	routes.HandleRequest()
 }
