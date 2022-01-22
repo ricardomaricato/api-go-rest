bfa15bdf4118f4d7f3ad42450d701c972d498727
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-22 17:16:43 -0300

    feat: busca personalidade por id

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 	models.Personalidades = []models.Personalidade{
-		{Nome: "nome 1", Historia: "Historia 1"},
-		{Nome: "nome 2", Historia: "Historia 2"},
+		{Id: 1, Nome: "nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "nome 2", Historia: "Historia 2"},
 	}
 
 	fmt.Println("Iniciando o servidor Resto com Go")
--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,14 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/ricardomaricato/api-go-rest/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmPersonalidade).Methods("Get")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
