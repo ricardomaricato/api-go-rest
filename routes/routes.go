cec6985b4fffbfd1615b25fd16bd93e714c74ba5
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-23 20:27:52 -0300

    feat: deletar personalidade

--- routes/routes.go
+++ routes/routes.go
@@ -15,5 +15,6 @@ func HandleRequest() {
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmPersonalidade).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaUmaPersonalidade).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
