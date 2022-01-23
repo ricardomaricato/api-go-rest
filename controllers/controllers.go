b6eef838dd85e1fdf7c0c686963644fc4da3cc3e
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-23 20:18:51 -0300

    feat: cria e deleta personalidade

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/ricardomaricato/api-go-rest/database"
 	"github.com/ricardomaricato/api-go-rest/models"
 )
 
@@ -15,16 +15,30 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(models.Personalidades)
+	var p []models.Personalidade
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
 }
 
 func RetornaUmPersonalidade(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
 
-	for _, personalidade := range models.Personalidades {
-		if strconv.Itoa(personalidade.Id) == id {
-			json.NewEncoder(w).Encode(personalidade)
-		}
-	}
+func CriaUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	var novaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade)
+	database.DB.Create(&novaPersonalidade)
+	json.NewEncoder(w).Encode(novaPersonalidade)
+}
+
+func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
 }
--- main.go
+++ main.go
@@ -4,17 +4,11 @@ import (
 	"fmt"
 
 	"github.com/ricardomaricato/api-go-rest/database"
-	"github.com/ricardomaricato/api-go-rest/models"
 	"github.com/ricardomaricato/api-go-rest/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "nome 1", Historia: "Historia 1"},
-		{Id: 2, Nome: "nome 2", Historia: "Historia 2"},
-	}
-
 	database.ConectaComBancoDeDados()
-	fmt.Println("Iniciando o servidor Resto com Go")
+	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
